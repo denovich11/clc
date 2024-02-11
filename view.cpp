7b7308934d5f246d56e62385412b0e396de3a6bd
denovich11 <158401007+denovich11@users.noreply.github.com>
2024-02-11 23:14:36 +0700

    ready
    base requierments of task are met, but
    MVP and calculator are not integrated fully. Logic of work yet contains in view.
    recomendation -test only using form: 1 variable-operation-second variable- compute. if not: remember- Bugs included.

--- view.cpp
+++ view.cpp
@@ -1,6 +1,6 @@
 #include "view.h"
 #include "ui_view.h"
-
+double num_first;
 View::View(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::View)
@@ -17,7 +17,7 @@ View::View(QWidget *parent)
     connect(ui->pushButton_8,SIGNAL(clicked()),this,SLOT(digits_numbers()));
     connect(ui->pushButton_9,SIGNAL(clicked()),this,SLOT(digits_numbers()));
 
-    /*connect(ui->pushButton_plusminus,SIGNAL(clicked()),this,SLOT(operations()));
+    connect(ui->pushButton_plusminus,SIGNAL(clicked()),this,SLOT(operations()));
     connect(ui->pushButton_percent,SIGNAL(clicked()),this,SLOT(operations()));
 
     connect(ui->pushButton_minus,SIGNAL(clicked()),this,SLOT(math_operations()));
@@ -28,7 +28,7 @@ View::View(QWidget *parent)
     ui->pushButton_divide->setCheckable(true);
     ui->pushButton_minus->setCheckable(true);
     ui->pushButton_multiply->setCheckable(true);
-    ui->pushButton_plus->setCheckable(true);*/
+    ui->pushButton_plus->setCheckable(true);
 
 }
 
@@ -51,7 +51,7 @@ void View::setCalcResult(Model::CalcResult result)
         break;
 
     case Model::Error:
-        // Переход на следующую секцию
+
     default:
         ui->result_show->setText(tr("<font color=\"red\">Ошибка при расчете!</font>"));
         break;
@@ -59,12 +59,48 @@ void View::setCalcResult(Model::CalcResult result)
 }
 void View::on_pushButton_equal_clicked()
 {
-    emit calcActionTriggered();
+    //emit calcActionTriggered();
+    double labelNumber, num_second;
+    QString new_label;
+    num_second=ui->lineEdit->text().toDouble();
+    if(ui->pushButton_plus->isChecked())
+    {
+        labelNumber=num_first+num_second;
+        new_label=QString::number(labelNumber, 'g', 15);
+        ui->result_show->setText(new_label);
+        ui->pushButton_plus->setChecked(false);
+    }
+    else if(ui->pushButton_minus->isChecked())
+    {
+        labelNumber=num_first-num_second;
+        new_label=QString::number(labelNumber, 'g', 15);
+        ui->result_show->setText(new_label);
+        ui->pushButton_plus->setChecked(false);
+    }
+    else if(ui->pushButton_divide->isChecked())
+    {
+        if(num_second==0)
+        {ui->lineEdit->setText(0);}
+        else
+        {
+        labelNumber=num_first/num_second;
+        new_label=QString::number(labelNumber, 'g', 15);
+        ui->result_show->setText(new_label);
+        ui->pushButton_plus->setChecked(false);
+        }
+    }
+    else if(ui->pushButton_multiply->isChecked())
+    {
+        labelNumber=num_first*num_second;
+        new_label=QString::number(labelNumber, 'g', 15);
+        ui->result_show->setText(new_label);
+        ui->pushButton_plus->setChecked(false);
+    }
 }
-void View::on_lineEdit_returnPressed()
+/*void View::on_lineEdit_returnPressed()
 {
     emit calcActionTriggered();
-}
+}*/
 void View::on_pushButton_dot_clicked()
 {
     if(!(ui->lineEdit->text().contains('.')))
@@ -74,7 +110,7 @@ void View::on_pushButton_dot_clicked()
 void View::digits_numbers()
 {
     QPushButton *button=(QPushButton *)sender();
-   /* double all_numbers;
+    double all_numbers;
     QString new_label;
 
     if(ui->lineEdit->text().contains(".") && button->text()=="0")
@@ -86,12 +122,12 @@ void View::digits_numbers()
         new_label=QString::number(all_numbers, 'g', 15);
     }
         ui->lineEdit->setText(new_label);
-    */
-   ui->lineEdit->setText(ui->lineEdit->text()+button->text());
+
+  // ui->lineEdit->setText(ui->lineEdit->text()+button->text());
 
 }
 
-/*void View::operations()
+void View::operations()
 {
     QPushButton *button=(QPushButton *)sender();
     double all_numbers;
@@ -110,37 +146,52 @@ void View::digits_numbers()
         ui->lineEdit->setText(new_label);
     }
 }
-*/
+void View::math_operations()
+{
+    QPushButton *button=(QPushButton *)sender();
+    num_first= ui->lineEdit->text().toDouble();
+    ui->lineEdit->setText("");
+    button->setChecked(true);
+
+}
+
 void View::on_pushButton_CE_clicked()
 {
- emit clearActionTriggered();
+ //emit clearActionTriggered();
+    ui->pushButton_plus->setChecked(false);
+    ui->pushButton_minus->setChecked(false);
+    ui->pushButton_multiply->setChecked(false);
+    ui->pushButton_divide->setChecked(false);
+    ui->result_show->setText("0");
+    ui->lineEdit->setText("0");
 }
 
 
-void View::on_pushButton_divide_clicked()
+/*void View::on_pushButton_divide_clicked()
 {
     ui->lineEdit->setText(ui->lineEdit->text()+"/");
-     emit divideActionTriggered();
+     //emit divideActionTriggered();
 }
 
 
 void View::on_pushButton_multiply_clicked()
 {
     ui->lineEdit->setText(ui->lineEdit->text()+"*");
-     emit multiplyActionTriggered();
+     //emit multiplyActionTriggered();
 }
 
 
 void View::on_pushButton_minus_clicked()
 {
     ui->lineEdit->setText(ui->lineEdit->text()+"-");
-     emit minusActionTriggered();
+     //emit minusActionTriggered();
 }
 
 
 void View::on_pushButton_plus_clicked()
 {
     ui->lineEdit->setText(ui->lineEdit->text()+"+");
-     emit plusActionTriggered();
+     //emit plusActionTriggered();
 }
 
+*/
--- view.h
+++ view.h
@@ -20,11 +20,11 @@ public:
     void setCalcResult(Model::CalcResult result);
 signals:
     void calcActionTriggered();
-    void clearActionTriggered();
-    void divideActionTriggered();
-    void multiplyActionTriggered();
-    void plusActionTriggered();
-    void minusActionTriggered();
+    //void clearActionTriggered();
+   // void divideActionTriggered();
+    //void multiplyActionTriggered();
+  //  void plusActionTriggered();
+    //void minusActionTriggered();
 private:
     Ui::View *ui;
 private slots:
@@ -34,14 +34,14 @@ private slots:
    //void on_result_show_windowTitleChanged(const QString &title);
     void on_pushButton_dot_clicked();
     void digits_numbers();
-    void on_lineEdit_returnPressed();
-   // void operations();
+    //void on_lineEdit_returnPressed();
+    void operations();
     void on_pushButton_CE_clicked();
-    //void math_operations();
-    void on_pushButton_divide_clicked();
-    void on_pushButton_multiply_clicked();
-    void on_pushButton_minus_clicked();
-    void on_pushButton_plus_clicked();
+    void math_operations();
+    //void on_pushButton_divide_clicked();
+    //void on_pushButton_multiply_clicked();
+    //void on_pushButton_minus_clicked();
+    //void on_pushButton_plus_clicked();
 };
 
 #endif // VIEW_H
